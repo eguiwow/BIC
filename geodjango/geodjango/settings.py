1ab0e101b1cf9fbba25d107d2cba5e0ac21b97e0
eguiwow <ander.eguiluz@opendeusto.es>
2021-02-09 13:23:39 +0100

    Comment added on settings.py

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -135,7 +135,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
     #os.path.join(BASE_DIR,'world','static'),
 )
+# This locations are local now but they should be relative in production
+# GDAL/GEOS_LIBRARY_PATH (only for Windows)
 if platform == "win32":
-# GDAL_LIBRARY_PATH (for Windows)
     GDAL_LIBRARY_PATH = r'C:\Program Files (x86)\release-1928-x64-gdal-3-2-1-mapserver-7-6-2\bin\gdal302'
     GEOS_LIBRARY_PATH=r'C:\Program Files (x86)\release-1928-x64-gdal-3-2-1-mapserver-7-6-2\bin\geos_c.dll'
