45d92975e3ebd2827edc74a53899734c8753534e
eguiwow <ander.eguiluz@opendeusto.es>
2021-03-01 09:50:09 +0100

    Añadidos GPX de Alfonso

--- bic/datacentre/load.py
+++ bic/datacentre/load.py
@@ -5,44 +5,19 @@
 import sys 
 from pathlib import Path
 from django.contrib.gis.utils import LayerMapping
-<<<<<<< HEAD
 from .models import GPX_track, GPX_trackpoint , GPX_waypoint
 
-=======
-from .models import GPX_track, GPX_trackpoint , GPX_waypoint #WorldBorder
-
-# world_mapping = {
-#     'fips' : 'FIPS',
-#     'iso2': 'ISO2',
-#     'iso3' : 'ISO3',
-#     'un' : 'UN',
-#     'name' : 'NAME',
-#     'area' : 'AREA',
-#     'pop2005' : 'POP2005',
-#     'region' : 'REGION',
-#     'subregion' : 'SUBREGION',
-#     'lon' : 'LON',
-#     'lat' : 'LAT',
-#     'mpoly' : 'MULTIPOLYGON',
-# }
-
->>>>>>> 35f09649dade736b577ca28ee4f68275cfc79f8d
 gpx_track_mapping = {
     'mlstring':'MultiLineString',
 }
 
 # world_shp = Path(__file__).resolve().parent / 'data' / 'TM_WORLD_BORDERS-0.3.shp'
-<<<<<<< HEAD
 #gpx_file_path = Path(__file__).resolve().parent / 'data' / 'gpx' / 'diego.gpx' 
-=======
-gpx_file_path = Path(__file__).resolve().parent / 'data' / 'gpx' / 'diego.gpx' 
->>>>>>> 35f09649dade736b577ca28ee4f68275cfc79f8d
 
 # Método que recorre la carpeta datacentre/data/gpx e introduce en la BD los tracks de los GPX 
 # TODO load no solo tracks sino trackpts, wpts, etc.
 def run(verbose=True):
     # LayerMapping --> Docs en https://docs.djangoproject.com/en/3.1/ref/contrib/gis/layermapping/
-<<<<<<< HEAD
     dir_gpx_data = Path("/home/eguiwow/github/BIC/bic/datacentre/data/gpx") # Directorio donde están de momento guardados los GPXs
     for filepath in dir_gpx_data.glob( '*.gpx' ):
         print(filepath)
@@ -63,11 +38,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-=======
-    # Ejemplo del tutorial:
-    # lm = LayerMapping(WorldBorder, str(world_shp), world_mapping, transform=False)
-    
-    
-    lm = LayerMapping(GPX_track, str(gpx_file_path), gpx_track_mapping, layer=2, transform=False)
-    lm.save(strict=False, verbose = verbose) # strict = True antes. Esto lo que hace es que si hay un fallo para la ejecución
->>>>>>> 35f09649dade736b577ca28ee4f68275cfc79f8d
