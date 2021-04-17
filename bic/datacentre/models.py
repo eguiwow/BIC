4c5d9e492e313a544fd450cce4c86b3870fb927e
eguiwow <ander.eguiluz@opendeusto.es>
2021-04-17 11:24:42 +0200

    SCK API call workiing

--- bic/datacentre/models.py
+++ bic/datacentre/models.py
@@ -1,77 +1,5 @@
 from django.contrib.gis.db import models
 
-########################################
-################ GPX ###################
-########################################
-
-# Fichero GPX fuente
-# desde https://github.com/jedie/django-for-runners/blob/master/src/for_runners/models/gpx.py
-class GPX_file(models.Model):
-    gpx_name = models.TextField(help_text="The raw gpx file name")
-    gpx_file = models.FileField(
-        verbose_name=("GPX File"),
-        upload_to='uploads/gpx/',
-        #storage=OverwriteFileSystemStorage(create_backups=True),
-        max_length=511,
-        null=True,
-        blank=True,
-    )
-    def __str__(self):
-        return self.gpx_name
-
-
-# Para punto INICIAL y FINAL del track
-class GPX_waypoint(models.Model):
-    # Django fields corresponding to 
-    # layer [waypoints] of gpx
-    name = models.CharField(max_length=50, null=True)
-    time = models.DateTimeField(null=True)
-    lon = models.FloatField(null=True)
-    lat = models.FloatField(null=True)
-
-    # GeoDjango-field <-> (Point)
-    point = models.PointField()
-
-    # Returns the string representation of the model.
-    def __str__(self):
-        return self.name
-
-# Colección de <trkpt>
-class GPX_track(models.Model):
-    # Django fields corresponding to 
-    # layer [tracks] of gpx
-    name = models.CharField(max_length=50, null=True)
-    start_time = models.DateTimeField(null=True)
-    end_time = models.DateTimeField(null=True) 
-    distance = models.FloatField(null=True)
-
-    # GeoDjango-field <-> (MultiLineString)
-    mlstring = models.MultiLineStringField()
-    
-    # Returns the string representation of the model.
-    def __str__(self):
-        return self.name
-
-# Points dentro de un track: <trkpt>
-class GPX_trackpoint(models.Model):
-    # Django fields corresponding to 
-    # layer [trackpoints] of gpx
-    name = models.CharField(max_length=50, null=True)
-
-    track_id = models.ForeignKey(GPX_track, on_delete=models.CASCADE)
-    # TODO Decidir si implementar segments 
-    # track_seg_id = models.ForeignKey(GPX_seg, on_delete=models.CASCADE)
-    lon = models.FloatField()
-    lat = models.FloatField()
-    ele = models.FloatField(null=True)
-    time = models.DateTimeField(null=True)
-
-    # GeoDjango-field <-> (Point)
-    point = models.PointField()
-
-    # Returns the string representation of the model.
-    def __str__(self):
-        return self.name
 
 
 ########################################
@@ -120,21 +48,98 @@ class SCK_device(models.Model):
     # TODO cómo meter tracks de dispositivo -> GPX_track u otro modelo    
     def __str__(self):
         return self.name
-    
+
+# Modelo que recoge los sensores de SCK + los que queramos meter en un futuro
+# Hay que proveer un ID diferente al que da SCK para sensores nuevos    
 class Sensor(models.Model):    
     sensor_id = models.IntegerField(primary_key=True) # ID provided by SCPlatform
-    device = models.ForeignKey(SCK_device, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    description = models.CharField(max_length=140)
 
     def __str__(self):
         return self.name
 
 class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    device = models.ForeignKey(SCK_device, on_delete=models.CASCADE)
+    time  = models.DateTimeField(null=True)
     value = models.FloatField(null=True)
     units = models.CharField(max_length=50)
     point = models.PointField()
-    time  = models.DateTimeField(null=True)
+    
+    def __str__(self):
+        return self.name
 
+########################################
+################ GPX ###################
+########################################
+
+# Fichero GPX fuente
+# desde https://github.com/jedie/django-for-runners/blob/master/src/for_runners/models/gpx.py
+class GPX_file(models.Model):
+    gpx_name = models.TextField(help_text="The raw gpx file name")
+    gpx_file = models.FileField(
+        verbose_name=("GPX File"),
+        upload_to='uploads/gpx/',
+        #storage=OverwriteFileSystemStorage(create_backups=True),
+        max_length=511,
+        null=True,
+        blank=True,
+    )
+    def __str__(self):
+        return self.gpx_name
+
+
+# Para punto INICIAL y FINAL del track
+class GPX_waypoint(models.Model):
+    # Django fields corresponding to 
+    # layer [waypoints] of gpx
+    name = models.CharField(max_length=50, null=True)
+    time = models.DateTimeField(null=True)
+    lon = models.FloatField(null=True)
+    lat = models.FloatField(null=True)
+
+    # GeoDjango-field <-> (Point)
+    point = models.PointField()
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
+
+# Colección de <trkpt>
+class GPX_track(models.Model):
+    # Django fields corresponding to 
+    # layer [tracks] of gpx
+    name = models.CharField(max_length=50, null=True)
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True) 
+    distance = models.FloatField(null=True)
+    device = models.ForeignKey(SCK_device, on_delete=models.CASCADE, null=True)
+
+    # GeoDjango-field <-> (MultiLineString)
+    mlstring = models.MultiLineStringField()
+    
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
+
+# Points dentro de un track: <trkpt>
+class GPX_trackpoint(models.Model):
+    # Django fields corresponding to 
+    # layer [trackpoints] of gpx
+    name = models.CharField(max_length=50, null=True)
+
+    track_id = models.ForeignKey(GPX_track, on_delete=models.CASCADE)
+    # TODO Decidir si implementar segments 
+    # track_seg_id = models.ForeignKey(GPX_seg, on_delete=models.CASCADE)
+    lon = models.FloatField()
+    lat = models.FloatField()
+    ele = models.FloatField(null=True)
+    time = models.DateTimeField(null=True)
+
+    # GeoDjango-field <-> (Point)
+    point = models.PointField()
+
+    # Returns the string representation of the model.
     def __str__(self):
         return self.name
--- bic/datacentre/sck_api.py
+++ bic/datacentre/sck_api.py
@@ -3,18 +3,7 @@ import requests
 from .models import SCK_device, Sensor, Measurement
 from django.contrib.gis.geos import Point
 
-# ID Kit de Ander = 13535
-# UUID de Ander = b4b060cb-b4bd-48f6-b60b-ac09799133db
-# ID Kit de Diego = 13524
-# UUID de Diego = 829e7acd-7f7a-4f19-9197-2e590561e741
-
-# 3 llamadas interesantes (destacables de momento):
-# -------------------------------------------------
-# Get Historical Readings (de un sensor en concreto) https://api.smartcitizen.me/v0/devices/:device_id/readings
-# CSV Archive of readings (mail con csv histórico - NEEDS AUTHENTICATION) https://api.smartcitizen.me/v0/devices/:id/readings/csv_archive
-# Get Latest Readings (de todo un kit) https://api.smartcitizen.me/v0/devices/:id
 def check_devices():
-    # TODO sacar devices y hacer llamadas a la API para cada device a ver si hay datos nuevos
     devices = SCK_device.objects.all()
     sensors = Sensor.objects.all()
     id_sensor_list = []
@@ -31,26 +20,32 @@ def check_devices():
             j1 = json.loads(resp.text)
             timestamp = j1["last_reading_at"]
             data = j1["data"]
+            location = data["location"]
+            sensor_point = None
+            if location:
+                sensor_point = Point(location["longitude"], location["latitude"])
             sensors_data = data["sensors"]
             for sensor in sensors_data:
                 id_sensor = sensor["id"]
-                name = sensor["name"]
+                sensor_name = sensor["name"]
+                sensor_description = sensor["description"]
                 value = sensor["value"]
                 unit = sensor["unit"]
                 if id_sensor not in id_sensor_list:
                     # Si no existe el tipo de sensor, crear sensor
-                    new_sensor = Sensor(sensor_id=id_sensor, device=device, name=sensor["name"])
+                    new_sensor = Sensor(sensor_id=id_sensor, name=sensor_name, description=sensor_description)
                     new_sensor.save()   
                 # Sacamos los datos para efectuar la nueva medida (Measurement)            
-                # TODO chequear si el measurement que comprobamos es nuevo
-                # Si es measurement nuevo --> creamos nuevo registro
-                new_meas = Measurement(sensor = Sensor.objects.get(pk=id_sensor), units=unit, value=value, point=Point(0,0), time=timestamp)
-                new_meas.save()
-                # Si no es measurement nuevo no hacemos nada
-                reading = {timestamp, id_sensor, name, value}
+                measurements = Measurement.objects.filter(sensor=Sensor.objects.get(pk=id_sensor)).filter(device=device).filter(time=timestamp) # Si es measurement nuevo --> creamos nuevo registro
+                if not measurements:# Si no es measurement nuevo no hacemos nada
+                    new_meas = Measurement(sensor = Sensor.objects.get(pk=id_sensor), device=device, units=unit, value=value, point=sensor_point, time=timestamp)
+                    new_meas.save()
+                else:
+                    print("NO HAY MEASUREMENTS NUEVOS") # TODO que esto se indique en la GUI
+                reading = {timestamp, id_sensor, sensor_name, value}
                 readings.append(reading)
         else:
-            print("NOT a 200 answer code") #TODO handle this exception
+            print("NOT a 200 answer code") #TODO handle this exception better
 
         print(readings)
         
